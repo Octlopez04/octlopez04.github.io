5e3ed11cfdbbf515f1a80774c2b71487a0d61aa0
Manolero232 <115741146+Manolero232@users.noreply.github.com>
2022-10-20 15:32:01 -0500

    rrrrrreglo

--- proy.php
+++ proy.php
@@ -4,13 +4,13 @@ $con=mysqli_connect('localhost','root','','admin');
 mysqli_set_charset($con,'utf8');
 
 //Pide los datos de usuario
-$a1=$_POST['Uss'];
+$corre=$_POST['corrr'];
+$Uss=$_POST['Uss'];
 $Pass=password_hash($Pass=$_POST['pas'].PASSWORD_DEFAULT,['cost'=>15]);
-$a2=$_POST['Pass'];
 
 
 //guarda los datos en sql y los sube
-$sql="INSERT INTO Usuarios (Usuarios,Contraseña) VALUES ('Uss','Pass')";
+$sql="INSERT INTO Usuarios (Correo,Usuarios,Contraseña) VALUES ('$corre','$Uss','$Pass')";
 $p=mysqli_query($con,$sql);
 
 
