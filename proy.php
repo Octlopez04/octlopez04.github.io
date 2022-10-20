52fb6989410104980070ec4539861ec572b16d87
Octlopez04 <114187056+Octlopez04@users.noreply.github.com>
2022-10-20 15:06:45 -0500

    arreglada la conexion

--- proy.php
+++ proy.php
@@ -1,15 +1,16 @@
 <?php
-require_once('con.php');
+//pide la conexion
+$con=mysqli_connect('localhost','root','','admin');
+mysqli_set_charset($con,'utf8');
 
+//Pide los datos de usuario
 $a1=$_POST['Uss'];
 $Pass=password_hash($Pass=$_POST['pas'].PASSWORD_DEFAULT,['cost'=>15]);
 $a2=$_POST['Pass'];
 
 
-////////
-
-$sql="INSERT INTO america (Usser, Pass) VALUES ('Uss','Pass')";
-
+//guarda los datos en sql y los sube
+$sql="INSERT INTO Usuarios (Usuarios,Contraseña) VALUES ('Uss','Pass')";
 $p=mysqli_query($con,$sql);
 
 
