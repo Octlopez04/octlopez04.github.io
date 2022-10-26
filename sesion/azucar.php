fc07aa7c2fa252ac17623e641a2a24c144ff7c6a
Octlopez04 <114187056+Octlopez04@users.noreply.github.com>
2022-10-26 14:24:53 -0500

    sabe

--- sesion/azucar.php
+++ sesion/azucar.php
@@ -1,14 +1,15 @@
 <?php
-//inicio de secion
+//inicio de sesion
 error_reporting(0);
 session_start();
      require_once('conx.php');
 $Uss=$_POST['Uss'];
 $Pass=$_POST['pass'];
 
-//pedir
+//pid eel usuario
 $sql="SELECT Uss, Pass FROM prvas Where Uss='$Uss'";
 
+//
 $query=mysqli_query($con, $sql);
 
 $rec=mysqli_fetch_array($query);
