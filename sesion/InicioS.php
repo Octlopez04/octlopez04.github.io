062aaf8e03326c08e5ed0d3452e4ad304c3c0b7f
Manolero232 <115741146+Manolero232@users.noreply.github.com>
2022-11-11 15:22:56 -0600

    Create InicioS.php
    inissssssio de sesion

---
+++ sesion/InicioS.php
@@ -0,0 +1,25 @@
+<?php
+//inicio de sesion
+session_start();
+     require_once('bombones.php');
+$Uss=$_POST['Uss'];
+$Pass=$_POST['pas'];
+
+//pid eel usuario
+
+$sql="SELECT Usuario, Contraseña FROM inicios Where Usuario = '$Uss'";
+
+//baja los datos de la tabla
+$envio=mysqli_query($con,$sql);
+//??¿¿
+$env=mysqli_fetch_array($envio);
+$hash=$env['Contraseña'];
+     //Verifica las contrañoñas
+     if(!password_verify($Pass,$hash)){
+          echo('no atranca');
+     }else{
+          echo('Bienvenido: '.$Uss );
+          $_SESSION['loggedin'];
+}
+
+?>
